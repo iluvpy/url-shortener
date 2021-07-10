5a48bafdc05605bb75bccb4adc875b1c931a4285
iluvpy <miomanns265@gmail.com>
2021-07-10 16:46:18 +0200

    removed unessesary closure

--- src/static/js/urls.js
+++ src/static/js/urls.js
@@ -1,13 +1,9 @@
 
 const del_url_id = (id) => {
     console.log("delete url was called!");
-    console.log(`del url id was called with id ${id}`)
-    const delete_url = () => {
-        
-        var hxr = new XMLHttpRequest();
-        hxr.open("GET", `/rurl/${id}`);
-        hxr.send(null);
-        location.reload();
-    }
-    return delete_url;
+    console.log(`del url id was called with id ${id}`);
+    var hxr = new XMLHttpRequest();
+    hxr.open("GET", `/rurl/${id}`);
+    hxr.send(null);
+    location.reload();
 }
